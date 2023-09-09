6bd3e1afe54e40f1abc82216fa7d197575b88c05
Taskeren <r0yalist@outlook.com>
2023-09-10 03:01:58 +0800

    updated publishing scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,11 @@
 plugins {
     java
     application
+    `maven-publish`
 }
 
 group = "explode"
-version = "1.0"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -33,5 +34,32 @@ tasks.jar {
         attributes(
             "Main-Class" to "cn.taskeren.explode3.caretaker.Main"
         )
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            val suffix =
+				if("${project.version}".endsWith("SNAPSHOT", ignoreCase = true)) { "snapshots" } else { "releases" }
+            url = uri("http://play.elytra.cn:31055/$suffix")
+
+            isAllowInsecureProtocol = true
+
+			credentials {
+				username = project.findProperty("repo.username")?.toString() ?: System.getenv("REPO_USERNAME")
+				password = project.findProperty("repo.password")?.toString() ?: System.getenv("REPO_PASSWORD")
+			}
+        }
+    }
+
+    publications {
+        create<MavenPublication>("TaskerenRepo") {
+            groupId = "cn.taskeren.explode3"
+            artifactId = "caretaker"
+            version = project.version.toString()
+
+            from(components["java"])
+        }
     }
 }
